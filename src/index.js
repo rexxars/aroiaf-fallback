1dfac1d22048bac5cd8aeb49151bd7a0147882a2
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-09-03 12:32:17 -0700

    fix: exit on internal server errors

--- src/index.js
+++ src/index.js
@@ -17,6 +17,11 @@ app.get('/', indexController)
 app.use((err, req, res, next) => {
   console.error(err)
   res.status(500).send('Internal Server Error!')
+
+  setImmediate(() => {
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  })
 })
 
 exports.app = app
